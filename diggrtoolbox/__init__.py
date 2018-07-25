740905ce281ef79b2f01d33099fc8090c293cbd4
Florian Rämisch <raemisch@ub.uni-leipzig.de>
2018-07-25 11:53:27 +0200

    added package docstring for diggrtoolbox

--- diggrtoolbox/__init__.py
+++ diggrtoolbox/__init__.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
+"""
+diggrtoolbox is the main package around all the small tools which were
+developed in the diggr group. Each tool is located in a separated subpackage.
+
+All tools are made available at package level, as every subpackage often only
+contains one class/function, separation into the subpackages appeared to be
+not the best idea.
+"""
 
 from .deepget import deepget
-
 from .treeexplore import TreeExplore, treehash
-
 from .zipaccess import ZipSingleAccess, ZipMultiAccess
 
 __all__ = ['deepget',
