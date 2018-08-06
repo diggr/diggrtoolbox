2f2ed82e35f87927889b120191830255c3698855
Florian Rämisch <raemisch@ub.uni-leipzig.de>
2018-08-06 15:45:12 +0200

    rolled back to setup.py and removed setup.cfg

--- setup.py
+++ setup.py
@@ -1,13 +1,31 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-try:
-    from setuptools import setup
-except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup
+from setuptools import setup, find_packages
+from os.path import dirname, abspath, join
+
+base_path = dirname(abspath(__file__))
+
+with open(join(base_path, "README.rst")) as readme_file:
+    readme = readme_file.read()
+
+with open(join(base_path, "requirements.txt")) as req_file:
+    requirements = req_file.readlines()
 
 setup(
-    # setup_requires=['setup.cfg'],
-    setup_cfg=True
+    name = "diggrtoolbox",
+    description = "diggrtoolbox provides a variety of tools to be used in game research and the digital humanities in general.",
+    long_description=readme,
+    license="GPL",
+    author = "Diggr Team",
+    author_email='team@diggr.link',
+    url='https://github.com/diggr/diggrtoolbox',
+    packages=find_packages(exclude=['dev', 'docs']),
+    package_dir={
+            'diggrtoolbox': 'diggrtoolbox'
+        },
+    version = "0.2",
+    install_requires=requirements,
+    test_suite="pytest",
+    include_package_data=True,
 )
