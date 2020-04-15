816bcff4a59ea78d20c6cece4632ce4822aede2e
Florian Rämisch <raemisch@ub.uni-leipzig.de>
2020-04-15 14:27:40 +0200

    Fix typo in standardize.__init__

--- diggrtoolbox/standardize/__init__.py
+++ diggrtoolbox/standardize/__init__.py
@@ -1,7 +1,7 @@
 from .standardize import remove_html, remove_bracketed_text, remove_punctuation, std_url, std
 
 __all__ = ['remove_html',
-           'remove_bracked_text',
+           'remove_bracketed_text',
            'remove_punctuation',
            'std_url',
            'std']
